0dbec8736b1efc98e811e620fd8002b2271666c6
Sasha <70911600+sasabeghiu@users.noreply.github.com>
2024-06-25 21:38:42 +0200

    db

--- dbconfig.php
+++ dbconfig.php
@@ -1,6 +1,15 @@
 <?php
-$type = "mysql";
-$servername = "mysql";
-$username = "root";
-$password = "secret123";
-$database = "developmentdb";
+$type = "pgsql";
+$servername = getenv("DB_HOST");
+$username = getenv("DB_USERNAME");
+$password = getenv("DB_PASSWORD");
+$database = getenv("DB_DATABASE");
+$port = getenv("DB_PORT");
+
+$dsn = "$type:host=$servername;port=$port;dbname=$database";
+
+try {
+    $pdo = new PDO($dsn, $username, $password, [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
+} catch (PDOException $e) {
+    die('Connection failed: ' . $e->getMessage());
+}
