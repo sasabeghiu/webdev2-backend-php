758127036d68db5b72577893f044e0e9287c3b52
Sasha <70911600+sasabeghiu@users.noreply.github.com>
2024-06-25 19:14:43 +0000

    tester

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => '__root__',
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
-        'reference' => null,
+        'pretty_version' => 'dev-hostbranch',
+        'version' => 'dev-hostbranch',
+        'reference' => '11e6191b0ab6f82a5cdb808653b50b8a521c8033',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         '__root__' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
-            'reference' => null,
+            'pretty_version' => 'dev-hostbranch',
+            'version' => 'dev-hostbranch',
+            'reference' => '11e6191b0ab6f82a5cdb808653b50b8a521c8033',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
